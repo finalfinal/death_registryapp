e7d84495a43aeeb2cda79cd107d6e4f00c7cacd5
bugatee <eric.amo.ef@gmail.com>
2018-01-04 17:02:13 +0000

    i edit the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SecondPage } from '../second/second';
+//import { SecondPage } from '../second/second';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +10,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) { }
   nextPage(){
-    this.navCtrl.push(SecondPage);
+    //this.navCtrl.push(SecondPage);
   }
 
 }
