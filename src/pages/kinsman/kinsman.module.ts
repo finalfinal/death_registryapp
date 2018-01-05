2a1262baa24e7922e768d1ef8b657d53a1fc28ad
Asare Samuel <asaresamuel@gmail.com>
2018-01-05 02:52:06 +0000

    done

---
+++ src/pages/kinsman/kinsman.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { KinsmanPage } from './kinsman';
+
+@NgModule({
+  declarations: [
+    KinsmanPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(KinsmanPage),
+  ],
+})
+export class KinsmanPageModule {}
